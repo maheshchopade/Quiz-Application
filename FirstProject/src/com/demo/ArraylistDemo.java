ee20fa12b9ab909da472c4d6609095c422efb865
anki01-comd <119093113+anki01-comd@users.noreply.github.com>
2025-03-29 16:25:44 +0530

    Arraylist Code for Adding city into list

---
+++ FirstProject/src/com/demo/ArraylistDemo.java
@@ -0,0 +1,20 @@
+package com.demo;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArraylistDemo {
+
+	public static void main(String[] args) {
+		
+	   List<String> list=new ArrayList<String>();
+	   list.add("pune");
+	   list.add("mumbai");
+	   list.add("banglore");
+	   list.add("hydrabad");
+	   
+	   System.out.println(list);
+	}
+
+}
