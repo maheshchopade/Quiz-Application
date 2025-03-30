c24f664071c61222ef3da36f636a80a3574b93d2
anki01-comd <119093113+anki01-comd@users.noreply.github.com>
2025-03-30 16:45:14 +0530

    ArrayList Program For names

---
+++ DemoPracticePeoject/src/com/practice/ArralistDemo.java
@@ -0,0 +1,22 @@
+package com.practice;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArralistDemo {
+
+	public static void main(String[] args) {
+	
+			
+			   List<String> list=new ArrayList<String>();
+			   list.add("Ankita");
+			   list.add("Nikita");
+			   list.add("Pooja");
+			   list.add("Vijay");
+			   
+			   System.out.println(list);
+			}
+
+	}
+
+
