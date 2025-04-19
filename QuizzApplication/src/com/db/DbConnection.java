7cae1cec9a6457c4315244ace1d95b6a8051e338
Ankita <ankitawankhade0108@gmail.com>
2025-04-20 00:29:46 +0530

    DataBase Connection

---
+++ QuizzApplication/src/com/db/DbConnection.java
@@ -0,0 +1,12 @@
+package com.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DbConnection {
+	public static Connection getConnection() throws Exception {
+		Class.forName("oracle.jdbc.driver.OracleDriver");
+		Connection con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "system", "system");
+		return con;
+	}
+}
